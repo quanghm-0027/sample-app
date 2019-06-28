e958651bedc8170c3e02750acfd1499feb64a942
Minh Quang <minhquang4334@gmail.com>
2019-06-28 11:25:50 +0700

    resolve comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
-# comment
-
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = t('title.base_title')
+    page_title.empty ? base_title : (page_title + " | " + base_title)
+  end
 end
