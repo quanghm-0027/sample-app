ba9c89f3e2b4befbf53316e5093cdcc12c8c3aa0
Minh Quang <minhquang4334@gmail.com>
2019-06-28 11:25:50 +0700

    complete chapter 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-                      locale : I18n.default_locale
+    compare_variable = I18n.available_locales.include?(locale)
+    I18n.locale = compare_variable ? locale : I18n.default_locale
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-
-  def full_title(page_title = '')
-    base_title = t('title.base_title')
-    page_title.empty ? base_title : (page_title + " | " + base_title)
+  # Returns the full title on a per-page basis. # Documentation
+  def full_title page_title = ""
+    base_title = t("title.base_title")
+    page_title.empty? ? base_title : (page_title + " | " + base_title)
   end
 end
